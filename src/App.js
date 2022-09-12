313330b01969d22ce1024dba97aa4f21ee5be943
Fei Li <feili4287@gmail.com>
2022-09-12 00:22:31 -0400

    render random quote when first open page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const friendsUrl = "https://friends-quotes-api.herokuapp.com/quotes/random";
+
+  const [post, setPost] = useState("");
+  const [author, setAuthor] = useState("");
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    getQuote();
+  }, []);
+
+  const getQuote = () => {
+    axios
+      .get(friendsUrl)
+      .then((response) => {
+        setPost(response.data.quote);
+        setAuthor(response.data.character);
+      })
+      .catch((error) => {
+        setError(error);
+      });
+  };
+
+  function handleClick() {
+    getQuote();
+  }
+  if (error) return `Error: ${error.message}`;
+
+  if (!post) return null;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <div id="quote-box">
+        <div id="text">{post}</div>
+        <div id="author">{author}</div>
+        <button id="new-quote" onClick={handleClick}>
+          new-quote
+        </button>
+        <a id="tweet-quote" href="www.google.com">
+          tweet
         </a>
-      </header>
+      </div>
     </div>
   );
 }
